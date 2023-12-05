04b9423f0d3e99f11c3322e18110a5575c2bd966
Maevey <70716966+callmeMaevey@users.noreply.github.com>
2023-12-04 20:46:35 -0500

    idk what this is doing tbh - im so rusty :(

--- 1.c
+++ 1.c
@@ -3,37 +3,26 @@
 #include <string.h>
 #include <ctype.h>
 
-int getFirst(char *line) {
+char getFirst(char *line) {
+    char retVal = '\0'; // -1 means not found
     char* value = calloc( sizeof(' '), strlen(line) );
     strcpy(value, line);
     int offset = 0;
-    int retVal = -1; // -1 means not found
-    while (offset < strlen(value)) {
+    while (offset < strlen(line) && retVal == '\0') {
         if ( isdigit( *(value + offset) )) {
-            retVal =  *(value + offset);
+            retVal = value[offset];
         }
         offset++;
     }
-    printf("%i", retVal );
+    free(value);
+    printf("%c\n", retVal);
     return retVal;
 }
 
-    int getLast(char *line) {
-    char *index = line;
-    int offset = strlen(line) - 1;
-    while (offset >= 0) {
-        if (isdigit( *(index + offset))) {
-            return *(index + offset);
-        }
-        offset--;
-    }
-
-    return -1; //errorcase number Not Found
-}
 
 int main() {
 
-    printf("%i", getFirst("your7mom"));
+    printf("%c\n", getFirst("6your7mom"));
 
     // printf("%i", getLast("your7mom"));
 
