84b945d9546d4e9bb96a9b928a88ba1111904d64
Maevey <70716966+callmeMaevey@users.noreply.github.com>
2023-12-04 21:51:37 -0500

    final cleanup

--- 1.c
+++ 1.c
@@ -1,3 +1,7 @@
+// Maeve K. Kennedy
+// Advent of Code 2023 Day 1
+// written Dec 4th (late start)
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,57 +9,46 @@
 
 char getFirst(char *line) {
     char retVal = '\0'; // \0 means not found
-    // char* value = calloc( sizeof(' '), strlen(line) );
-    // strcpy(value, line);
     int offset = 0;
     while (offset < strlen(line) && retVal == '\0') {
         if ( isdigit( *(line + offset) )) {
             retVal = line[offset];
         }
         offset++;
-    }
-    // free(value);
+    }//walks forward until digit char is found
     return retVal;
 }
 
 char getLast(char *line) {
     char retVal = '\0'; // \0 means not found
-    // char* value = calloc( sizeof(' '), strlen(line) );
-    // strcpy(value, line);
     int offset = strlen(line);
     while (offset > 0 && retVal == '\0') {
         --offset;
         if ( isdigit( *(line + offset) )) {
             retVal = line[offset];
         }
-    }
-    // free(value);
+    }//walks backwards until digit is found
     return retVal;
 }
 
-
-
 int main(int argc, char** argv) {
-
-    if(argc < 2){ return -1; }
-
-//file setup
+    
+    if(argc < 2){ printf("call with path as argv[1]\n"); return -1; } // prevents errors
     int maxlen = 64;
     char *line = (char *)malloc(sizeof(char) * maxlen);
 
-    FILE* file = fopen( argv[1], "r");
-    if(file==NULL){
-        printf("filenotfound\n");
-        return -1;
-    }
+    //file setup
+    FILE* file = fopen( argv[1], "r");//open readonly
+    if(file==NULL){ printf("file not found\n"); return -1; } //catch fnf
+
     char values[2];
     int total = 0;
-    while(fgets(line, maxlen, file)){
+    while(fgets(line, maxlen, file)){ //while next line exists, copy it into &line
 
         values[0] = getFirst(line);
         values[1] = getLast(line);
-        printf("%i\n",atoi(values));
-        total+=atoi(values);
+        total+=atoi(values); //this is cause C is such a cool language
+
     }
     fclose(file);
     free(line);
