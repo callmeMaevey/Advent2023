b9329260146e312adb4b686e223c46aad772acfc
Maevey <70716966+callmeMaevey@users.noreply.github.com>
2023-12-05 00:51:22 -0500

    final cleanup for pt 2

--- 1.c
+++ 1.c
@@ -1,6 +1,7 @@
 // Maeve K. Kennedy
 // Advent of Code 2023 Day 1
 // written Dec 4th (late start)
+// I cant believe I solved part 2 
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,48 +10,49 @@
 
 char getNumberFromWord(char* section){
     char out = '\0';
-    
-    //jk not doing part 2
-    //oops Im doing it
+   //needs to be broken out into serach lenghts to prevent segfaults
+   //so three length words
     if(strlen(section)>=3){
         char* names[3][2] = {{"one","1"}, {"two","2"}, {"six","6"}};
         for(int index = 0; index<3; index++){
-            if(strncmp(section, *(names+index)[0], sizeof(char)*3)==0){
-                out = *(names[index])[1];
+            if(strncmp(section, *(names+index)[0], sizeof(char)*3)==0){ //compares 3 chars
+                out = *(names[index])[1]; //derefrenece the indexth value of names and access its 1st value
             }
         }
     }
-    
+
+    //then 4 lenght words
     if(strlen(section)>=4){
         char* names[4][2] = {{"four","4"}, {"five","5"}, {"nine","9"}};
         for(int index = 0; index<3; index++){
-            if(strncmp(section, *(names+index)[0], sizeof(char)*4)==0){
-                out = *(names[index])[1];
+            if(strncmp(section, *(names+index)[0], sizeof(char)*4)==0){ //compares 4 chars
+                out = *(names[index])[1]; 
             }
         }
     }
+
     // what the hell am I writing right now...
-    
+    //then 5 lenght words
     if(strlen(section)>=5){
-        char* names[5][2] = {{"three","3"},  {"seven","7"}, {"eight","8"}};
+        char* names[5][2] = {{"three","3"},  {"seven","7"}, {"eight","8"}}; //compares 5 chars
         for(int index = 0; index<3; index++){
             if(strncmp(section, *(names+index)[0], sizeof(char)*5)==0){
                 out = *(names[index])[1];
             }
         }
     }
-
-    return out;
+    return out; // just like the other functions, \0 impiles not found
 }
 
 char getFirst(char *line) {
     char retVal = '\0'; // \0 means not found
     int offset = 0;
-    while (offset < strlen(line) && retVal == '\0') {
+    while (offset < strlen(line) && retVal == '\0') { 
         if ( isdigit( *(line + offset) )) {
             retVal = line[offset];
         }
-        else{ retVal = getNumberFromWord(line+offset); }
+        else{ retVal = getNumberFromWord(line+offset); } 
+        // getNubmerFromWord returns \0 on fail so the loop keeps looping.
         offset++;
     }//walks forward until digit char is found
     return retVal;
