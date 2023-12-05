5e2c6ff1170605b484336cd909b46ece2399cb0d
Maevey <70716966+callmeMaevey@users.noreply.github.com>
2023-12-04 19:47:25 -0500

    first pass at funcitons

--- 1.c
+++ 1.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int getFirst(char* line){
     size_t len=strlen(line);
+    for (int i = 0; i<len; i++){
+        if(isdigit(line[i])){
+            return line[i];
+        }
+    }
+    return -1; //fail case 
 }
 
-int getLast(char* Line){
-
+int getLast(char* line){
+    size_t len=strlen(line);
+    for (int i = len; i>0; --i){
+        if(isdigit(line[i])){
+            return line[i];
+        }
+    }
+    return -1; //fail case 
 }
 
 
-void main(){
+int main(){
 
+    printf("%i", getFirst("your7mom"));
+    printf("%i", getLast("your7mom"));
 
-
-
-
+    return 0;
 }
