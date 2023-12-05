811267f137de67cfaa3c2b830fb82246eb90d4bb
Maevey <70716966+callmeMaevey@users.noreply.github.com>
2023-12-04 20:30:48 -0500

    forgot how much you have to c in c

--- 1.c
+++ 1.c
@@ -1,32 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
-int getFirst(char* line){
-    size_t len=strlen(line);
-    for (int i = 0; i<len; i++){
-        if(isdigit(line[i])){
-            return line[i];
+int getFirst(char *line) {
+    char* value = calloc( sizeof(' '), strlen(line) );
+    strcpy(value, line);
+    int offset = 0;
+    int retVal = -1; // -1 means not found
+    while (offset < strlen(value)) {
+        if ( isdigit( *(value + offset) )) {
+            retVal =  *(value + offset);
         }
+        offset++;
     }
-    return -1; //fail case 
+    printf("%i", retVal );
+    return retVal;
 }
 
-int getLast(char* line){
-    size_t len=strlen(line);
-    for (int i = len; i>0; --i){
-        if(isdigit(line[i])){
-            return line[i];
+    int getLast(char *line) {
+    char *index = line;
+    int offset = strlen(line) - 1;
+    while (offset >= 0) {
+        if (isdigit( *(index + offset))) {
+            return *(index + offset);
         }
+        offset--;
     }
-    return -1; //fail case 
+
+    return -1; //errorcase number Not Found
 }
 
-
-int main(){
+int main() {
 
     printf("%i", getFirst("your7mom"));
-    printf("%i", getLast("your7mom"));
+
+    // printf("%i", getLast("your7mom"));
 
     return 0;
 }
